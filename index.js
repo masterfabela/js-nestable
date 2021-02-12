b238c878e44aafc0bb45a7e6a7b7967071e6eabe
Jose Ramon <joseviromay@gmail.com>
2021-02-12 09:58:09 +0100

    Version con solo lo necesario

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 $(document).ready(function () {
-	/*var updateOutput = function (e) {
-		var list = e.length ? e : $(e.target),
-			output = list.data('output');
-		if (window.JSON) {
-			output.val(window.JSON.stringify(list.nestable('serialize'))); //, null, 2));
-		} else {
-			output.val('JSON browser support required for this demo.');
-		}
-	};*/
 
-	// activate Nestable for list 2
 	$('#nestable3').nestable({
 		group: 1,
 	}); /*
-		.on('change', updateOutput);*/
 
-	// output initial serialised data
-	updateOutput($('#nestable3').data('output', $('#nestable3-output')));
-
-	$('#nestable3').nestable();
 });
